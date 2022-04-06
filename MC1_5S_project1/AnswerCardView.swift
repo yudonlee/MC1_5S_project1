90d0c94b43dac7642e6b5baefc6913c504505057
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-04-06 10:15:54 +0900

    AnswerCardVIew commit

---
+++ MC1_5S_project1/AnswerCardView.swift
@@ -0,0 +1,20 @@
+//
+//  AnswerCardView.swift
+//  MC1_5S_project1
+//
+//  Created by yudonlee on 2022/04/06.
+//
+
+import SwiftUI
+
+struct AnswerCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnswerCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerCardView()
+    }
+}
