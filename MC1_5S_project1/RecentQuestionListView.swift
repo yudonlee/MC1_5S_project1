ee7e07d3c6b1ec6216035f6398e1e419bc8a2e67
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-04-06 10:17:43 +0900

    Second Line views upload

---
+++ MC1_5S_project1/RecentQuestionListView.swift
@@ -0,0 +1,20 @@
+//
+//  RecentQuestionListView.swift
+//  MC1_5S_project1
+//
+//  Created by yudonlee on 2022/04/06.
+//
+
+import SwiftUI
+
+struct RecentQuestionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecentQuestionListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentQuestionListView()
+    }
+}
