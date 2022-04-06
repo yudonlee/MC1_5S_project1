9025aece9792ff2d9f254cad1e1508bfc0063573
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-04-06 09:44:03 +0900

    Initial Commit

---
+++ MC1_5S_project1/MC1_5S_project1App.swift
@@ -0,0 +1,17 @@
+//
+//  MC1_5S_project1App.swift
+//  MC1_5S_project1
+//
+//  Created by yudonlee on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct MC1_5S_project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
