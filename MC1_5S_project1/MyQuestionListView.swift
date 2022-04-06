f73ec8dad9d5a4e5247b3ba7095a7b87da1c5a53
Wonhui Kim <0512tini@gmail.com>
2022-04-06 12:37:30 +0900

    segmented controller commit

--- MC1_5S_project1/MyQuestionListView.swift
+++ MC1_5S_project1/MyQuestionListView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct MyQuestionListView: View {
+    @State private var MaterialType = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+            Text("#익명\n sample data")
+        }
     }
 }
 
