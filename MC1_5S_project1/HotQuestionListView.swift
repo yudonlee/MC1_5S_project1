7365fbc9c332e3debc36794d5327501e6008da8f
lululll <95920991+lululll@users.noreply.github.com>
2022-04-07 01:24:33 +0900

    [search bar. 리스트, segmented controller추가] 아래는 앞으로 할일입니다. 리스트 어떻게 받아올지, search bar를 통한 리스트 검색기능 추가(이때, 답변이 있는 질문과 없는 질문 섹션으로 어떻게 나눌 수 있을까) 컨트롤러 전환시 리스트 재정렬 글쓰기 버튼 추가

--- MC1_5S_project1/HotQuestionListView.swift
+++ MC1_5S_project1/HotQuestionListView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
-struct HotQuestionListView: View {
+// examples_QnA
+struct examplesQuestions : View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text ("@Keily\n출입증은 어디서 수령하나요?")
+            .font(.system(size:15))
+    }
+}
+// SwitchOnBest
+//struct ButtonToLists : View {
+//    var body : some View {
+//
+//    }
+//}
+//RoundedRectangle(cornerRadius: 19)
+//    .stroke(Color.gray, lineWidth: 1.5)
+//    .frame(width: 350, height: 600)
+struct HotQuestionListView: View {
+    @State private var myMenu = 0
+    init() {
+        UITableView.appearance().backgroundColor = .white
+    }
+    // items in RoundedRectangle...
+    var body: some View {
+        NavigationView {
+            VStack {
+                //SelectedHotPage
+                Picker("", selection: $myMenu) {
+                    Text("Hot").tag(0)
+                    Text("New").tag(1)
+                }.pickerStyle(.segmented).frame(width: 300, alignment: .center)
+                
+                    .frame(height:40)
+                    .searchable(text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=$text@*/.constant("")/*@END_MENU_TOKEN@*/, placement: /*@START_MENU_TOKEN@*/.toolbar/*@END_MENU_TOKEN@*/)
+                List (0..<30) {_ in
+                    examplesQuestions()
+                }
+                //                .frame(width: 300, height: 600)
+                //                .padding(15)
+                //            .offset(x: -35.0, y: -50.0)
+                //                .border(Color.gray/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/2)
+                //                .cornerRadius(19.0)
+                
+                
+                
+                //            .frame(width: 350)
+            }
+        }
     }
 }
 
