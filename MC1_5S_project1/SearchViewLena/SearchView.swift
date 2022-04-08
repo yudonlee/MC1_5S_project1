59fbf0977073bd92803f0a31b8fe9ae531e14678
Lena <99120199+lenamin@users.noreply.github.com>
2022-04-08 11:36:02 +0900

    add SearchResultDetailView

--- MC1_5S_project1/SearchViewLena/SearchView.swift
+++ MC1_5S_project1/SearchViewLena/SearchView.swift
@@ -8,8 +8,85 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var text : String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SearchBar(text: self.$text)
+        SearchResultAnswerDetailView(postContents: postContentList)
+        SearchResultNoAnswerDetailView(postContents: postContentList)
+    }
+}
+
+struct SearchResultAnswerDetailView: View {
+    var postContents: [PostContent]
+    var body: some View {
+        // title
+        HStack {
+            Text("답변이 있는 질문")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(red: 0.333, green: 0.358, blue: 0.362))
+                .padding(.bottom, 1)
+                .padding(.leading, 15)
+            Spacer()
+        } // HStack
+        
+        // content section
+        VStack {
+            ScrollView(.vertical) {
+                VStack {
+                    ForEach(postContents) { post in
+                        HStack {
+                            Text("#\(post.name) \n \(post.title) (\(post.answerCount))")
+                            Spacer()
+                        }
+                        Divider()
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 5)
+                } // VStack
+            } // ScrollView
+        }
+        // border
+        .overlay(RoundedRectangle(cornerRadius: 19).stroke(Color.gray, lineWidth: 1))
+        .padding(.horizontal, 15)
+    }
+}
+        
+
+struct SearchResultNoAnswerDetailView: View {
+    var postContents: [PostContent]
+    var body: some View {
+        // title
+        HStack {
+            Text("답변이 없는 질문")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(red: 0.333, green: 0.358, blue: 0.362))
+                .padding(.bottom, 1)
+                .padding(.leading, 15)
+            Spacer()
+        } // HStack
+        
+        // content section
+        VStack {
+            ScrollView(.vertical) {
+                VStack {
+                    ForEach(postContents) { post in
+                        HStack {
+                            Text("#\(post.name) \n \(post.title) (0)")
+                            Spacer()
+                        }
+                        Divider()
+                    }
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 5)
+                } // VStack
+            } // ScrollView
+        }
+        // border
+        .overlay(RoundedRectangle(cornerRadius: 19).stroke(Color.gray, lineWidth: 1))
+        .padding(.horizontal, 15)
     }
 }
 
