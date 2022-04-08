3c97a9aeaedea23b93acfd39d58d7a99ac16b81c
Lena <99120199+lenamin@users.noreply.github.com>
2022-04-08 12:50:14 +0900

    add new file

--- MC1_5S_project1/SearchViewLena/SearchAnswerDetailView copy.swift
+++ MC1_5S_project1/SearchViewLena/SearchAnswerDetailView copy.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct SearchView: View {
+struct SearchAnswerDetailView: View {
     @State var text : String = ""
 
     var body: some View {
@@ -56,44 +56,10 @@ struct SearchResultAnswerDetailView: View {
 }
         
 
-struct SearchResultNoAnswerDetailView: View {
-    var postContents: [PostContent]
-    var body: some View {
-        // title
-        HStack {
-            Text("답변이 없는 질문")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundColor(Color(red: 0.333, green: 0.358, blue: 0.362))
-                .padding(.bottom, 1)
-                .padding(.leading, 15)
-            Spacer()
-        } // HStack
-        
-        // content section
-        VStack {
-            ScrollView(.vertical) {
-                VStack {
-                    ForEach(postContents) { post in
-                        HStack {
-                            Text("#\(post.name) \n \(post.title) (0)")
-                            Spacer()
-                        }
-                        Divider()
-                    }
-                    .padding(.horizontal, 15)
-                    .padding(.vertical, 5)
-                } // VStack
-            } // ScrollView
-        }
-        // border
-        .overlay(RoundedRectangle(cornerRadius: 19).stroke(Color.gray, lineWidth: 1))
-        .padding(.horizontal, 15)
-    }
-}
 
-struct SearchView_Previews: PreviewProvider {
+
+struct SearchAnswerDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchView()
+        SearchAnswerDetailView()
     }
 }
