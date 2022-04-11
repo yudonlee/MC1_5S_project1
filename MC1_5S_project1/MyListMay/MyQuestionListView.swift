5b9ecf58c2714f3ca710ec0b2a97b6a37231fc23
Wonhui Kim <0512tini@gmail.com>
2022-04-11 14:52:02 +0900

    edit my list

--- MC1_5S_project1/MyListMay/MyQuestionListView.swift
+++ MC1_5S_project1/MyListMay/MyQuestionListView.swift
@@ -11,38 +11,46 @@
 import SwiftUI
 
 struct MyQuestionListView: View {
-    var postContents: [PostContent]
     var body: some View {
         VStack {
             ScrollView(.vertical, showsIndicators: true){ //막대바 숨김-false
                 VStack(spacing: 15) {
-                    ForEach(postContents) { post in
-                        HStack {
-                            VStack(alignment: .leading, spacing: 10) {
-                                Text(post.name) //작성자 닉네임
-                                    .bold()
-                                Text(post.title) //게시글 내용
-                            }
-                            Spacer()
-                            HStack(spacing: 3) {
-                                Image(systemName: "bubble.right")
-                                Text(post.answerCount) //댓글 개수
-                            }
-                            
-                        }
+                    ForEach(postContentList) { post in
+                        let post_index = Int(post.index) ?? 0
+                        NavigationLink(destination: QuestionDetailView(index: post_index-1)){MyQuestionContentView(post: post)
+                        }.navigationBarTitleDisplayMode(.inline)
                         Divider()
                     }
                 }.padding(EdgeInsets(top: 10, leading: 30, bottom: 0, trailing: 30))
             }
         }.overlay(RoundedRectangle(cornerRadius: 19).stroke(Color.gray, lineWidth: 1))
             .padding()
-        
+    }
+}
+
+struct MyQuestionContentView: View { //질문 게시글 리스트
+    @State var post: PostContent
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(post.name) //작성자 닉네임
+                    .bold()
+                Text(post.title) //게시글 내용
+            }
+            Spacer()
+            HStack(spacing: 3) {
+                Image(systemName: "bubble.right")
+                Text(post.answerCount) //댓글 개수
+            }
+        }.foregroundColor(.black)
+            .multilineTextAlignment(.leading)
     }
 }
 
 struct MyQuestionListView_Previews: PreviewProvider {
     static var previews: some View {
-        MyQuestionListView(postContents: postContentList)
+        MyQuestionListView()
     }
 }
  
