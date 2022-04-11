5f2652e5d108434f4d0ba55337b76812dd0f23df
lululll <95920991+lululll@users.noreply.github.com>
2022-04-11 09:26:06 +0900

    HOT/NEW_케일리_세팅중입니다

--- MC1_5S_project1/MainQuestionKeily/RecentQuestionListView.swift
+++ MC1_5S_project1/MainQuestionKeily/RecentQuestionListView.swift
@@ -7,11 +7,63 @@
 
 import SwiftUI
 
+// SwitchOnBest
+//struct ButtonToAsk : View {
+//    var body : some View {
+//        Button(action: {
+//        }){
+//            Image(systemName: "square.and.pencil")
+//        }
+//    }
+//}
+
 struct RecentQuestionListView: View {
+    @State private var myMenu = 0
+    init() {
+        UITableView.appearance().backgroundColor = .white
+    }
+    // items in RoundedRectangle...
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Spacer()
+                //SelectHotOrNew
+                HStack{
+                    Picker("", selection: $myMenu) {
+                        Text("Hot").tag(0)
+                        Text("New").tag(1)
+                    }
+                    .frame(width: 250, height:40)
+                    .pickerStyle(.segmented)
+                    .offset(x: -23)
+                    ButtonToAsk()
+                }
+                ScrollView(.vertical) {
+                    ForEach(postContentList) {
+                        answer in
+                        VStack(alignment:.leading) {
+                            HStack(spacing: 3) {
+                                Text("[\(answer.name)]\t" + answer.title)
+                                Spacer()
+                                Image(systemName: "bubble.right")
+                                Text(answer.answerCount) //댓글 개수
+                            }
+                            Divider()
+                        }
+                    }.searchable(text: /*@PLACEHOLDER=$text@*/.constant("")/*@END_MENU_TOKEN@*/, placement: /*@START_MENU_TOKEN@*/.automatic)
+                    .padding(.all, 15)
+                        
+                }
+                // border
+                .overlay(RoundedRectangle(cornerRadius: 19).stroke(Color.gray, lineWidth: 1))
+                .padding([.leading, .bottom, .trailing], 15)
+            }
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+        }
     }
 }
+
 
 struct RecentQuestionListView_Previews: PreviewProvider {
     static var previews: some View {
