4eeb92e2c64d68cb2aa8dbd4b265d9883895e6e1
mth9406 <mth9406@naver.com>
2022-04-11 09:26:47 +0900

    first commit

--- MC1_5S_project1/AnswerCardPasta/QuestionCardView.swift
+++ MC1_5S_project1/AnswerCardPasta/QuestionCardView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct QuestionCardView: View {
+    
+    @State var comment:String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+            VStack{
+                Spacer()
+                Text("질문 작성")
+                    .font(.system(size: 15, design: .default))
+                    .frame(width: UIScreen.screenWidth, height: 50,alignment: .center)
+                    .padding(15)
+                
+                ZStack
+                {
+                    RoundedRectangle(cornerRadius: 19)
+                        .stroke(Color.gray, lineWidth: 2)
+                    
+                    TextField("답변 작성", text: $comment)
+                        .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                            .padding()
+                }
+            }
+            Button(action: {
+                print("버튼 누름!")
+            }){
+                Text("완료")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(7)
+                    .border(MINTCOLOR, width: 1)
+            }
+            .background(MINTCOLOR)
+            .cornerRadius(8)
+            .frame(width: 100, height: 100)
+            .position(x: UIScreen.screenWidth-10, y: 17)
+        }
     }
 }
 
 struct QuestionCardView_Previews: PreviewProvider {
     static var previews: some View {
         QuestionCardView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
+let MINTCOLOR = Color(red: 48 / 255, green: 176 / 255, blue: 199 / 255)
+//UIScreen.screenWidth
+//UIScreen.screenHeight
