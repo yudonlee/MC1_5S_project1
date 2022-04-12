81987988e4f257cc7dc1a993246850fd4d3f971e
Wonhui Kim <0512tini@gmail.com>
2022-04-12 21:53:13 +0900

    merge mylistpickerview and mypageview to mypageview

--- MC1_5S_project1/MyListMay/MyPageView.swift
+++ MC1_5S_project1/MyListMay/MyPageView.swift
@@ -1,5 +1,5 @@
 //
-//  MyPageView.swift
+//  MyListPickerView.swift
 //  MC1_5S_project1
 //
 //  Created by 김원희 on 2022/04/08.
@@ -8,12 +8,96 @@
 import SwiftUI
 
 struct MyPageView: View {
+    
+    @State var searchText = ""
+
+    @State private var selectedSide: SideOfMenu = .myQues
+    @State var text : String = ""
+    
     @EnvironmentObject var viewModel: postViewModel
 
     var body: some View {
-        VStack {
+        NavigationView {
+            VStack {
+                MyProfileView()
+                Searching(text: $searchText)
+                    .padding(.top, -10)
+                    .padding(15)
+                
+                Picker("Choose a menu", selection: $selectedSide) {
+                    ForEach(SideOfMenu.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }.pickerStyle(.segmented)
+                    .padding()
+                
+                ChosenView(selectedSide: selectedSide)
+            }
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct Searching: View {
+    @Binding var text: String
+    @State private var isEditing = false
+    @FocusState private var isFocused: Bool
+    
+    var body: some View {
+        
+        HStack {
+            TextField("검색", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(.white)
+                .cornerRadius(8)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
             
-            MyListPickerView()
+        } // Hstack
+        .overlay(
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 8)
+                
+                if isEditing { // x를 누르면 내용을 지우고 키보드를 숨긴다
+                    Button(action: {
+                        self.text = ""
+                        hideKeyboard()
+                    }) {
+                        Image(systemName: "multiply.circle.fill")
+                            .foregroundColor(.gray)
+                            .padding(.trailing, 8)
+                    }
+                }
+            } // HStack
+        ) //overlay
+    }
+}
+    
+        
+enum SideOfMenu: String, CaseIterable {
+    case myQues = "내 질문"
+    case myAns = "내 답변"
+    case myScr = "스크랩"
+}
+
+struct ChosenView: View {
+    var selectedSide: SideOfMenu
+
+    var body: some View {
+        switch selectedSide {
+        case .myQues:
+            MyQuestionListView()
+        case .myAns:
+            MyAnswerListView()
+        case .myScr:
+            MyScrapListView()
         }
     }
 }
@@ -23,3 +107,14 @@ struct MyPageView_Previews: PreviewProvider {
         MyPageView()
     }
 }
+
+
+// 키보드를 닫기 위해 UIKit에서 불러온다
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
