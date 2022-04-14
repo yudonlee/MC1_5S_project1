16bccebb6c01d20ec240361a4ff0aadf6ce7641e
mth9406 <mth9406@naver.com>
2022-04-14 09:25:10 +0900

    spacer

--- MC1_5S_project1/AnswerCardPasta/AnswerCardView.swift
+++ MC1_5S_project1/AnswerCardPasta/AnswerCardView.swift
@@ -84,7 +84,9 @@ struct AnswerCardView: View {
                     .frame(width: UIScreen.screenWidth * 0.9, height: 10, alignment: .center)
                     .padding(10)
                 ScrollView(.horizontal){
-                    HStack{
+                    HStack(alignment:.center){
+                        Spacer()
+                            .padding(.leading, 30)
                         // ForEach Envirnoment object 넣어주기
                         ForEach(viewModel.postContents){
                             post in
